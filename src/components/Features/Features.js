510d7b265b0ec1479be8fa6fa96ad588d940b71f
danialcodes <md.danial.islam@g.bracu.ac.bd>
2021-10-04 17:31:43 +0600

    Updated

--- src/components/Features/Features.js
+++ src/components/Features/Features.js
@@ -1,29 +1,29 @@
-// import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
-// import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar as fasStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import "./Features.css"
 // import { useState } from 'react/cjs/react.development';
 import Rating from 'react-rating';
 const Features = (props) => {
     const { star, features } = props;
-    // const rating = []
-    // for (let i = 0; i < star; i++) {
-    //     // <i className="fas color-star fa-star"></i>
-    //     rating.push(<FontAwesomeIcon key={"fas" + i} className="color-star" icon={fasStar} />)
-    // }
-    // for (let i = 0; i < (5 - star); i++) {
-    //     // <i className="far fa-star"></i>
-    //     rating.push(<FontAwesomeIcon key={"far" + i} className="color-star" icon={farStar} />)
-    // }
+    const rating = []
+    for (let i = 0; i < star; i++) {
+        // <i className="fas color-star fa-star"></i>
+        rating.push(<FontAwesomeIcon key={"fas" + i} className="color-star" icon={fasStar} />)
+    }
+    for (let i = 0; i < (5 - star); i++) {
+        // <i className="far fa-star"></i>
+        rating.push(<FontAwesomeIcon key={"far" + i} className="color-star" icon={farStar} />)
+    }
     return (
         <div className="features">
             
             <div className="rating">
-                {/* {rating} */}
-                <Rating emptySymbol="fa fa-star-o color-star"
+                {rating}
+                {/* <Rating emptySymbol="fa fa-star-o color-star"
                 fullSymbol="fa fa-star color-star" initialRating={star}
-                readonly/>
+                readonly/> */}
             </div>
             <div>
                 <h3>Features</h3>
